f118a75f431c4163f6eca95ea75c95d42d5d2870
jx319 <103572456+jx319@users.noreply.github.com>
2023-10-01 15:11:35 +0200

    delete unused stuff

--- nixos/start-wl.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellScriptBin "start-wl"''
-        ${pkgs.swww}/bin/swww init &
-        ${pkgs.swww}/bin/swww img ~/.config/hypr/nix-wallpaper-dracula.png &
-
-        #${pkgs.waybar}/bin/waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
-        ${pkgs.dunst}/bin/dunst
-''
