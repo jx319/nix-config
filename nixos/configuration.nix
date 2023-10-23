121909f44b73b76dd7187a4ed4acf01beb590aa3
jx319 <103572456+jx319@users.noreply.github.com>
2023-10-23 19:21:04 +0200

    add helix cachix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,8 +133,14 @@
   };
   
   nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://helix.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      ];
   };
   
   environment.sessionVariables = {
