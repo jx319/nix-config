87d6ab5ecf7383c732e291b2f4acdad05adeb831
jx319 <103572456+jx319@users.noreply.github.com>
2023-11-01 14:58:04 +0100

    tofi launcher config

--- home/tofi/default.nix
+++ home/tofi/default.nix
@@ -1,15 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-	xdg.configFile."tofi/config".source = (pkgs.formats.ini {}).generate {
-		anchor = "top";
-		width = "100%";
-		height = "30";
-		horizontal = true;
-		font-size = 16;
-		font = "${pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }}/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFontPropo-Regular.ttf";
-		prompt-text = ''""'';
-		background-color = "#1e1e2e";
-		selection-color = "#a6e3a1";
-	};
+	xdg.configFile."tofi/config".text = ''
+		anchor = top
+		width = 100%
+		height = 45
+		horizontal = true
+		font-size = 16
+		font = ${pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; }}/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFontPropo-Regular.ttf
+		prompt-text = ">"
+		background-color = #1e1e2e
+		selection-color = #a6e3a1
+		outline-width = 0
+		border-width = 0
+		padding-top = 5
+		padding-bottom = 5
+		padding-left = 5
+		padding-right = 5
+		result-spacing = 25
+		drun-launch = true'';
 }
