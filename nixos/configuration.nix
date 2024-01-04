ccff72237d557406b68a85e36c6e5c6521278f0b
jx319 <103572456+jx319@users.noreply.github.com>
2024-01-04 17:55:11 +0100

    console theming

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -30,6 +30,29 @@
     #plymouth.enable = true; doesn't work with Linux 6.6 for some reason
     kernelPackages = pkgs.linuxPackages_latest;
   };
+
+  console = {
+      colors = [
+      "1e1e2e" # base
+      "f38ba8" # red
+      "a6e3a1" # green
+      "f9e2af" # yellow
+      "89b4fa" # blue
+      "f5c2e7" # pink
+      "94e2d5" # teal
+      "bac2de" # subtext1
+      "585b70" # surface2
+      "f38ba8" # red
+      "a6e3a1" # green
+      "f9e2af" # yellow
+      "89b4fa" # blue
+      "f5c2e7" # pink
+      "94e2d5" # teal
+      "a6adc8" # subtext0 
+    ];
+    earlySetup = true;
+    keyMap = "de";
+  };
   
   networking = {
   	hostName = "nixos";
