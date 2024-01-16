fcd88093bcad946ef0e68441ffae74eea16c9f92
jx319 <103572456+jx319@users.noreply.github.com>
2024-01-16 17:38:37 +0100

    use the new theme

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: 
+{ pkgs, inputs, ... }: 
 {
   programs.alacritty = {
     enable = true;
     settings = {
       import = [
-        pkgs.catppuccin-alacritty-theme.catppuccin-mocha
+        inputs.alacritty-theme.packages.${pkgs.system}.catppuccin_mocha
       ];
 
       font.normal.family = "JetBrains Mono Nerd Font";
