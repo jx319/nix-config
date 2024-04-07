9632df4693734b97234de1309b7c34e4c27cd37f
jx319 <103572456+jx319@users.noreply.github.com>
2024-04-07 13:09:39 +0200

    qt6 theming

--- home/qt/default.nix
+++ home/qt/default.nix
@@ -1,21 +1,24 @@
 { pkgs, ... }:
 {  
-  qt = {
-      enable = true;
-      platformTheme = "qtct";
-      style = {
-        name = "kvantum";
-      };
-  };
+  # qt = {
+  #   enable = true;
+  #   platformTheme = "qtct";
+  #   style = {
+  #     name = "Lightly";
+  #     package = pkgs.kdePackages.callPackage ../../pkgs/lightly-qt6 { };
+  #   };
+  # };
   
-  xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" { 
-    General.theme = "Catppuccin-Mocha-Green";
-  };
+  # xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" { 
+  #   General.theme = "Catppuccin-Mocha-Green";
+  # };
   
-  xdg.configFile."qt5ct/qt5ct.conf".source = (pkgs.formats.ini {}).generate "qt5ct.conf" {
+  xdg.configFile."qt6ct/qt6ct.conf".source = (pkgs.formats.ini {}).generate "qt6ct.conf" {
     Appearance = {
       icon_theme = "Papirus-Dark";
-      style = "kvantum";
+      style = "Lightly";
+      color_scheme_path = "${./Catppuccin-Mocha.conf}";
+      custom_palette = true;
     };
     Fonts = {
       fixed = ''"JetBrainsMono Nerd Font,12,-1,5,50,0,0,0,0,0,Regular"'';
@@ -23,10 +26,10 @@
     };
   };
 
+  xdg.configFile."qt6ct/colors/Catppuccin-Mocha.conf".source = ./Catppuccin-Mocha.conf;
+
   home.packages = with pkgs; [
-    (catppuccin-kvantum.override {
-      accent = "Green";
-      variant = "Mocha";
-    })
+    (pkgs.kdePackages.callPackage ../../pkgs/lightly-qt6 { })
+    qt6ct
   ];
 }
